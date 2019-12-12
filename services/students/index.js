c1df978320f3cba3c530ba237f68bb71b51fd226
Faizan Bardai <faizan.bardai@celeritas-solutions.com>
2019-12-12 01:31:03 +0100

    student via ID; check email availability

--- services/students/index.js
+++ services/students/index.js
@@ -17,8 +17,9 @@ const writeFile = (filePath, student) => {
   fs.writeFileSync(filePath, buffer);
 };
 
+const studentsArray = readFile(filePath);
+
 router.get("/", (req, res) => {
-  const studentsArray = readFile(filePath);
   const studentsNameAndId = studentsArray.map(student => {
     let container = {
       fullName: `${student.name} ${student.surname}`,
@@ -29,6 +30,33 @@ router.get("/", (req, res) => {
   res.send(studentsNameAndId);
 });
 
+router.get("/:id", (req, res) => {
+  const studentID = req.params.id;
+  const student = studentsArray.find(
+    student => student._id === parseInt(studentID)
+  );
+  res.send(student);
+});
+
+router.get(
+  "/checkEmail/:email",
+  [
+    check("email")
+      .isEmail()
+      .normalizeEmail()
+      .withMessage("Email is not valid!")
+  ],
+  (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(422).json({ errors: errors.array() });
+    }
+    const email = req.params.email;
+    let answer = studentsArray.filter(student => student.email === email);
+    return res.send(answer.length ? false : true);
+  }
+);
+
 router.post(
   "/",
   [
@@ -37,30 +65,31 @@ router.post(
       .normalizeEmail()
       .withMessage("Email is not valid!"),
     check("name")
-      .exists({checkNull: true, checkFalsy: true})
+      .exists({ checkNull: true, checkFalsy: true })
       .isLength({ min: 2 })
       .withMessage("Name must contain atlease 2 characters!"),
     check("surname")
-      .exists({checkNull: true, checkFalsy: true})
-      .withMessage("Surname must be provided!"),    
+      .exists({ checkNull: true, checkFalsy: true })
+      .withMessage("Surname must be provided!"),
     check("dob")
       .isISO8601()
       .isBefore()
-      .withMessage("Date of birth should be formatted as per ISO8601 and must be before today!")
+      .withMessage(
+        "Date of birth should be formatted as per ISO8601 and must be before today!"
+      )
   ],
   (req, res) => {
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
       return res.status(422).json({ errors: errors.array() });
     }
-    let allStudents = readFile(filePath);
     let student = {
       ...req.body,
-      _id: allStudents.length + 1,
+      _id: parseInt(studentsArray.length + 1),
       createdAt: new Date()
     };
-    allStudents.push(student);
-    writeFile(filePath, allStudents);
+    studentsArray.push(student);
+    writeFile(filePath, studentsArray);
     let container = {
       fullName: `${student.name} ${student.surname}`,
       ID: student._id
